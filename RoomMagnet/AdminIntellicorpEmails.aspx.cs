1ea47b59ccde07c6ff102968d469849f4393f01f
ramakrat <43589549+ramakrat@users.noreply.github.com>
2019-12-05 20:15:12 -0500

    added new tableau and added email functionality

---
+++ RoomMagnet/AdminIntellicorpEmails.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminIntellicorpEmails : System.Web.UI.Page
+{
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        if (Session["type"] != null)
+        {
+            if ((int)Session["type"] == 1)
+            {
+                this.MasterPageFile = "~/AdminPage.master";
+            }
+            else if ((int)Session["type"] == 2)
+            {
+                this.MasterPageFile = "~/HostPage.master";
+            }
+            else if ((int)Session["type"] == 3)
+            {
+                this.MasterPageFile = "~/TenantPage.master";
+            }
+        }
+        else if (Session["type"] == null)
+        {
+            this.MasterPageFile = "~/MasterPage.master";
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["AccountId"] != null && Convert.ToInt16(Session["type"]) == 1)
+        {
+            System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["roommagnetdbConnectionString"].ToString());
+
+            System.Data.SqlClient.SqlCommand selectEmail = new System.Data.SqlClient.SqlCommand();
+            selectEmail.Connection = sc;
+
+            sc.Open();
+
+            intellicorpEmail.Text = String.Empty;
+
+            //Populate Emails
+            selectEmail.CommandText = "SELECT Account.FirstName, Account.LastName, Account.Email " +
+                "FROM Account FULL OUTER JOIN Host ON Account.AccountID = Host.HostID FULL OUTER JOIN Tenant ON Account.AccountID = Tenant.TenantID " +
+                "WHERE Host.BackgroundCheckStatus = 0 OR Tenant.BackgroundCheckStatus = 0";
+
+
+            StringBuilder nameCard = new StringBuilder();
+            System.Data.SqlClient.SqlDataReader reader = selectEmail.ExecuteReader();
+
+            while (reader.Read())
+            {
+                String fName = reader["FirstName"].ToString();
+                String lName = reader["LastName"].ToString();
+                String email = reader["Email"].ToString();
+
+                //StringBuilder
+                StringBuilder myCard = new StringBuilder();
+                myCard.Append("<tr><td><a href =\"#\" class=\"tenantdashlist\">" + "Name: " + fName + " " + lName + "</a></td>" +
+                    "<td><a href =\"#\" class=\"tenantdashlist\">" + "Tenant: " + email + "</a></td></tr>");
+                intellicorpEmail.Text += myCard.ToString();
+            }
+            reader.Close();
+            sc.Close();
+        }
+        else
+        {
+            Response.Redirect("Home.aspx");
+        }
+    }
+    protected void btnChange_Click(object sender, EventArgs e)
+    {
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["roommagnetdbConnectionString"].ToString());
+
+        System.Data.SqlClient.SqlCommand selectEmail = new System.Data.SqlClient.SqlCommand();
+        selectEmail.Connection = sc;
+
+        sc.Open();
+
+        //Populate Emails
+        selectEmail.CommandText = "UPDATE Tenant SET BackgroundCheckStatus = 1 WHERE BackgroundCheckStatus = 0; " +
+            "UPDATE Host SET BackgroundCheckStatus = 1 WHERE BackgroundCheckStatus = 0;";
+        selectEmail.ExecuteNonQuery();
+        Page_Load(sender, e);
+    }
+}
--- RoomMagnet/HostDashboard.aspx
+++ RoomMagnet/HostDashboard.aspx
@@ -56,7 +56,6 @@
                                         <button style="color: #000;" type="button" class="btn  dropdown-item js-scroll-trigger" data-toggle="modal" data-dismiss="modal" data-target="#addRoom">
                                         Add Room</>
                                     </div>
-
                                 </div>
 
                                 <!--ADD ROOM MOD-->
@@ -202,9 +201,9 @@
                             </div>
                         </div>
                     </div>
-                    <!--END OF MESSAGE MOD-->
-
                 </div>
+                <!--END OF MESSAGE MOD-->
+
                 <div class="col-md-6">
                     <!--TENANT/HOST MOD-->
                     <div class="col-md-12">
@@ -216,7 +215,7 @@
                                 </ul>
                                 <!--BUTTON-->
                                 <div class=" text-center">
-                                    <a href="Host-Tenants.aspx" class="btn btn-info">View more</a>
+                                    <a href="#" class="btn btn-info">View more</a>
                                 </div>
                                 <!--END OF BUTTON-->
                             </div>
